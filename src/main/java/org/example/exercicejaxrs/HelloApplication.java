4090daebaa4011c78ef068976cc3b57baa6f4962
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-17 15:52:12 +0200

    Initial commit

---
+++ src/main/java/org/example/exercicejaxrs/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.exercicejaxrs;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
